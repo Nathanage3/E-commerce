709106df91b2681de55d864a32309f1d0ba00b10
Nathanage3 <sisay954@gmail.com>
2024-08-28 00:26:42 +0300

    dammydata

--- mando/mando/settings.py
+++ mando/mando/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
