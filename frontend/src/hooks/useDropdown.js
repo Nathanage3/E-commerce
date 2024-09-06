5e79489df4b636353689b7b0104be0569133255c
Nathan Hailu <118963179+Nathanage3@users.noreply.github.com>
2024-09-06 21:52:20 +0300

    Delete frontend directory
    remove dir

--- frontend/src/hooks/useDropdown.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-const useDropdown = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleDropdown = () => setIsOpen((prev) => !prev);
-  return [isOpen, toggleDropdown];
-};
-
-export default useDropdown;
