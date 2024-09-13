a5f1eb3ad7ce50b1561421a55e30c1f271e02739
Nathanage3 <sisay954@gmail.com>
2024-09-14 00:36:44 +0300

    new update

---
+++ new.jsx
@@ -0,0 +1,32 @@
+{
+    id: 11,
+    updatedDate: new Intl.DateTimeFormat('en-IN', {
+      dateStyle: 'long',
+    }).format(new Date()),
+    createdBy: 'Binyam Techan',
+    category: 'Development',
+    title: 'The Complete JavaScript Course 2024: From Zero to Expert!',
+    subTitle:
+      'The modern JavaScript course for everyone! Master JavaScript with projects, challenges and theory. Many courses in one!',
+    objectives: [
+      'Become an advanced, confident, and modern JavaScript developer from scratch',
+      'Become job-ready by understanding how JavaScript really works behind the scenes',
+      'JavaScript fundamentals: variables, if/else, operators, boolean logic, functions, arrays, objects, loops, strings, etc.',
+      'Modern OOP: Classes, constructors, prototypal inheritance, encapsulation, etc.',
+    ],
+    detail:
+      'The modern JavaScript course for everyone! Master JavaScript with projects, challenges and theory. Many courses in one!',
+    prerequisites:
+      'No coding experience is necessary to take this course! I take you from beginner to expert!',
+    courseFor: 'No coding experience is necessary to take this course!',
+    level: 'intermidiate',
+    videoFile: cardImg5,
+    img: cardImg5,
+    price: 39.99,
+    oldPrice: 79.99,
+    currency: 'USD',
+    students: 0,
+    rating: 0,
+    ratingCount: 0,
+    courseDuration: 1000000,
+  },
