c2a3848e4b6a28feb3f51914dc334980bb3582e7
Nathanage3 <sisay954@gmail.com>
2024-09-20 01:52:20 +0300

    ready to deploy

--- mando/settings.py
+++ mando/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-d3xp9rrm^qe-8x(-tg0xy@(xr0z+$!%j5bjtn+tx@2l)o0#e5e
 # DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 DEBUG = True
 
-DJANGO_ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "")
+DJANGO_ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "mando.koyeb.app")
 print(DJANGO_ALLOWED_HOSTS) 
 
 ALLOWED_HOSTS = DJANGO_ALLOWED_HOSTS.split(",")
