af5bb125338d5391cecb0519c541c5650a5fd38f
Nathanage3 <sisay954@gmail.com>
2024-09-19 14:39:20 +0300

    cart

--- courses/apps.py
+++ courses/apps.py
@@ -1,6 +1,7 @@
+# In courses/apps.py
 from django.apps import AppConfig
-import os
 
 class CoursesConfig(AppConfig):
     name = 'courses'
-    path = os.path.join(os.path.dirname(__file__), 'courses')
+    label = 'my_courses'
+
