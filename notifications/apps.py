90049d5325853494f1da1f93735ea124154fe8f3
Nathanage3 <sisay954@gmail.com>
2024-09-19 09:09:15 +0300

    update settings

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,6 +1,6 @@
+# notifications/apps.py
 from django.apps import AppConfig
-import os
 
 class NotificationsConfig(AppConfig):
     name = 'notifications'
-    path = os.path.join(os.path.dirname(__file__), 'notifications')
+    path = '/workspace/notifications'  # Use the correct path here
