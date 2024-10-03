a61c1c85c333788e039ba66607f9628376ace0ea
Nathanage3 <sisay954@gmail.com>
2024-10-03 23:51:33 +0300

    good job

--- mando/settings.py
+++ mando/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from datetime import timedelta
 
+APPEND_SLASH=False
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
