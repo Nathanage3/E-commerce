18fbea737786425c787f0bc2dfe863d602325577
Nathanage3 <sisay954@gmail.com>
2024-10-14 13:20:12 +0300

    update requirements

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.postgres.fields import ArrayField
 from decimal import Decimal
 from django.core.validators import MinValueValidator, FileExtensionValidator, \
     MaxValueValidator
