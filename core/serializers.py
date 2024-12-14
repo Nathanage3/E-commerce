df57a7a6799aea842e6af9b9ceda9fd69255c94b
Dagim Sisay <sisay954@gmail.com>
2024-12-14 21:58:44 +0300

    new update

--- core/serializers.py
+++ core/serializers.py
@@ -10,16 +10,16 @@ class UserCreateSerializer(BaseUserCreateSerializer):
     role = serializers.ChoiceField(choices=User.ROLE_CHOICES, required=True)
     class Meta(BaseUserCreateSerializer.Meta):
         model = User  # Ensure this points to your custom User model
-        fields = ['id', 'email', 'password', 'first_name', 'last_name', 'role']
+        fields = ['email', 'password', 'first_name', 'last_name', 'role']
 
 
 class UserSerializer(BaseUserSerializer):
     profile_picture = serializers.ImageField(read_only=True)
-    id = serializers.IntegerField(read_only=True)
+   
     #role = serializers.ChoiceField(choices=User.ROLE_CHOICES, required=True)
     class Meta(BaseUserSerializer.Meta):
         model = User  # Ensure this points to your custom User model
-        fields = ['id', 'email', 'first_name', 'last_name', 'role', 'profile_picture']
+        fields = ['email', 'first_name', 'last_name', 'role', 'profile_picture']
 
 
 class SetPasswordSerializer(BaseSetPasswordSerializer):
